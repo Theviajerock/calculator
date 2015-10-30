74875c0d42c29384824f69cea895a74562059d7f
Victor Hernandez <victorliferock@gmail.com>
2015-10-29 22:33:14 -0500

    Added all the stylesheet and functionality

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 var total = 0;
+var totalString;
 var tempNum;
 var opeExpresion;
-
+//Function that receive the push button of the numbers
 function pushNumber(num) {
 	if (total === 0) {
 		total = num.toString();
@@ -9,28 +10,38 @@ function pushNumber(num) {
 		console.log(total);
 		console.log(tempNum);
 	} else if (typeof(total) === "string") {
-        if (total[total.length - 1] == "*" || total[total.length - 1] == "/" || total[total.length - 1] == "+" || total[total.length - 1] == "-") {
-			total = total + num;
+        
+			total = total + num.toString();
 			console.log(total);
 			console.log(tempNum);
-			document.getElementById('result').value = total;
-		}
-	};
+			document.getElementById('result').value = total
+		
+	}else if(total[total.length-1] == '.' ){}
 };
 
+//Function that receive the push button of the operators
 function operation(operator) {
 	if (typeof(total) != "undefined") {
-		if (total[total.length - 1] != "*" || total[total.length - 1] != "/" || total[total.length - 1] != "+" || total[total.length - 1] != "-") {
-			total = total + operator
-		}
+		if (total[total.length - 1] == "*" || 
+			total[total.length - 1] == "/" || 
+			total[total.length - 1] == "+" || 
+			total[total.length - 1] == "-") {
+		}else if(total[total.length-1] == '.' ) {
+
+		}else{total = total + operator;}
 	}
 };
 
 
 function equal() {
-	makeIt = eval(total);
-	console.log(makeIt);
-	total = makeIt;
-	document.getElementById('result').value = total;
+	var makeTheOperation = eval(total);
+	total = makeTheOperation;
+	document.getElementById('result').value = total.toFixed(2);
 	
+};
+
+function clearResult() {
+  total = 0;
+  document.getElementById('result').value = total
+
 };
